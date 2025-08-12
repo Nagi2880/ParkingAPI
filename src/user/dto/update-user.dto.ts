cfa8621ef9a45ef479f2a935e67fca43a3112dbe
Nagi2880 <alexanderamaral17@gmail.com>
2025-08-11 21:59:23 -0300

    update user dto uploaded

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -2,7 +2,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    updatedAt?: Date;
+    updatedAt: Date;
     isActive?: boolean;
     deleted?: boolean;
     phone?: string;
